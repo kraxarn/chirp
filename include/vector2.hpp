ae23978b8b24750994b30e6b97893da4ac676c72
kraxarn <kraxie@protonmail.com>
2023-10-15 21:46:16 +0200

    Add vector2

---
+++ include/vector2.hpp
@@ -0,0 +1,106 @@
+#pragma once
+
+namespace chirp
+{
+	/**
+	 * 2-dimensional vector
+	 * @tparam T Contained type
+	 */
+	template<typename T>
+	class vector2
+	{
+	public:
+		/**
+		 * Construct a vector2 with default values
+		 */
+		vector2() = default;
+
+		/**
+		 * Construct a vector2 with specified values
+		 * @param x X
+		 * @param y Y
+		 */
+		vector2(const T x, const T y)
+			: x(x), y(y)
+		{
+		}
+
+		/**
+		 * Convert a vector2 to another type
+		 * @tparam Target Target type
+		 * @return Vector2 of target type
+		 */
+		template<typename Target>
+		auto to() const -> vector2<Target>
+		{
+			return {
+				static_cast<Target>(x),
+				static_cast<Target>(y),
+			};
+		}
+
+		/**
+		 * Multiply x and y with specified value
+		 * @param value Value to multiply with
+		 * @return New vector with multiplied values
+		 */
+		auto operator*(T value) const -> vector2<T>
+		{
+			return {x * value, y * value};
+		}
+
+		/**
+		 * Divide x and y with specified value
+		 * @param value Value to divide by
+		 * @return New vector with divided values
+		 */
+		auto operator/(T value) const -> vector2<T>
+		{
+			return {x / value, y / value};
+		}
+
+		/**
+		 * Add value to vector
+		 * @param value Value to add
+		 * @return New vector with added values
+		 */
+		auto operator+(T value) const -> vector2<T>
+		{
+			return vector2<T>(x + value, y + value);
+		}
+
+		/**
+		 * Subtract value from vector
+		 * @param value Value to subtract
+		 * @return New vector with subtracted values
+		 */
+		auto operator-(T value) const -> vector2<T>
+		{
+			return vector2<T>(x - value, y - value);
+		}
+
+		/**
+		 * Multiply x and y with x and y of specified value
+		 * @param value Vector to multiply with
+		 * @return New vector with multiplied values
+		 */
+		auto operator+(const vector2<T> &value) const -> vector2<T>
+		{
+			return {x + value.x, y + value.y};
+		}
+
+	private:
+		const T x;
+		const T y;
+	};
+
+	/**
+	 * A vector2 of float values
+	 */
+	using vector2f = vector2<float>;
+
+	/**
+	 * A vector2 of int values
+	 */
+	using vector2i = vector2<int>;
+}
