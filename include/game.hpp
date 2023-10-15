97dd259088eccb9f4ea16d0db71d1fb434a672ef
kraxarn <kraxie@protonmail.com>
2023-10-15 21:46:25 +0200

    Add game

---
+++ include/game.hpp
@@ -0,0 +1,96 @@
+#pragma once
+
+#include "color.hpp"
+
+namespace chirp
+{
+	/**
+	 * The basics for any game, creates a window and updates the contents of it
+	 */
+	class game
+	{
+	public:
+		/**
+		 * Run game
+		 */
+		void run();
+
+	protected:
+		/**
+		 * Load various game assets
+		 */
+		virtual void load_content() = 0;
+
+		/**
+		 * Update game logic
+		 * @param delta Time in seconds since the last frame
+		 * @note No guarantee how often it will be called
+		 */
+		virtual void update(float delta) = 0;
+
+		/**
+		 * Draw game content
+		 */
+		virtual void draw() = 0;
+
+		/**
+		 * Create a new window and context
+		 */
+		game();
+
+		/**
+		 * Close window and release resources
+		 */
+		~game();
+
+		/**
+		 * Exit game and close window
+		 */
+		void exit();
+
+		/**
+		 * Get target FPS
+		 * @return FPS
+		 */
+		[[nodiscard]]
+		auto get_target_fps() const -> int;
+
+		/**
+		 * Set target FPS
+		 * @param fps FPS
+		 * @note Defaults to monitor refresh rate
+		 */
+		void set_target_fps(int fps);
+
+	private:
+		/**
+		 * Default window width
+		 */
+		static constexpr int default_width = 1280;
+
+		/**
+		 * Default window height
+		 */
+		static constexpr int default_height = 720;
+
+		/**
+		 * Default window title
+		 */
+		static constexpr const char *default_title = "game window - chirp";
+
+		/**
+		 * Game is running
+		 */
+		bool running;
+
+		/**
+		 * Target FPS
+		 */
+		int target_fps;
+
+		/**
+		 * Clear color for each frame
+		 */
+		color clear_color;
+	};
+}
---
+++ src/game.cpp
@@ -0,0 +1,63 @@
+#include "game.hpp"
+#include "monitor.hpp"
+
+#include "raylib.h"
+
+chirp::game::game()
+	: running(false),
+	target_fps(0),
+	clear_color(chirp::color::black())
+{
+	InitWindow(default_width, default_height, default_title);
+
+	const auto monitor_index = chirp::monitor::current();
+	set_target_fps(chirp::monitor::refresh_rate(monitor_index));
+}
+
+chirp::game::~game()
+{
+	CloseWindow();
+}
+
+void chirp::game::run()
+{
+	running = true;
+	while (running)
+	{
+		update(GetFrameTime());
+
+		BeginDrawing();
+
+		ClearBackground({
+			clear_color.red(),
+			clear_color.green(),
+			clear_color.blue(),
+			clear_color.alpha(),
+		});
+
+		draw();
+
+		EndDrawing();
+
+		if (WindowShouldClose())
+		{
+			running = false;
+		}
+	}
+}
+
+void chirp::game::exit()
+{
+	running = false;
+}
+
+auto chirp::game::get_target_fps() const -> int
+{
+	return target_fps;
+}
+
+void chirp::game::set_target_fps(int fps)
+{
+	target_fps = fps;
+	SetTargetFPS(fps);
+}
