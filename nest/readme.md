9a728db72ccdc50aecfce88e939892e680e69de4
kraxarn <kraxie@protonmail.com>
2024-04-20 22:34:06 +0200

    Initial asset file builder

---
+++ nest/readme.md
@@ -0,0 +1,3 @@
+# nest builder
+
+Build multiple assets into a single file used by the engine, like how a bird builds a nest.
---
+++ nest/src/main.cpp
@@ -0,0 +1,137 @@
+#include <cstring>
+#include <filesystem>
+#include <format>
+#include <fstream>
+#include <iostream>
+#include <vector>
+
+#include "nlohmann/json.hpp"
+
+void print_help(const char *path)
+{
+	std::cout << "usage: " << path << " [options] assets\n\n"
+		<< "options:\n"
+		<< "  -p, --pack    pack assets\n"
+		<< "  -u, --unpack  unpack assets\n"
+		<< "  -o, --output  output content to stdout\n";
+}
+
+auto read_file(const std::filesystem::path &path) -> std::vector<char>
+{
+	std::ifstream file(path, std::ios::binary);
+
+	file.seekg(0, std::ios::end);
+	const auto filesize = file.tellg();
+	file.seekg(0, std::ios::beg);
+
+	std::vector<char> buffer(filesize);
+	file.read(buffer.data(), filesize);
+	return buffer;
+}
+
+auto pack(const char *input) -> int
+{
+	const std::filesystem::path path(input);
+	if (!exists(path) || !is_directory(path))
+	{
+		std::cerr << "error: directory does not exist!\n";
+		return 2;
+	}
+
+	nlohmann::json json;
+
+	for (const auto &entry: std::filesystem::recursive_directory_iterator(path))
+	{
+		if (!entry.is_regular_file())
+		{
+			continue;
+		}
+
+		const auto &entry_path = entry.path();
+
+		const auto &folder = entry_path.parent_path().stem().string();
+		const auto &filename = entry_path.filename().string();
+
+		if (!json.contains(folder))
+		{
+			json[folder] = nlohmann::json::object();
+		}
+
+		json[folder][filename] = read_file(entry_path);
+		std::cout << "< " << folder << '/' << filename << '\n';
+	}
+
+	std::filesystem::path output(path.parent_path() / path.stem());
+	output.replace_extension("nest");
+	std::cout << "> " << output << '\n';
+
+	const auto data = nlohmann::json::to_msgpack(json);
+	std::ofstream file(output);
+	file.write(reinterpret_cast<const char *>(data.data()), static_cast<std::streamsize>(data.size()));
+
+	return 0;
+}
+
+auto unpack(const char *path) -> int
+{
+	return 0;
+}
+
+auto output(const char *input) -> int
+{
+	const std::filesystem::path path(input);
+	if (!exists(path) || !is_regular_file(path))
+	{
+		std::cerr << "error: file does not exist!\n";
+		return 2;
+	}
+
+	const auto data = read_file(path);
+	const auto json = nlohmann::json::from_msgpack(data);
+
+	nlohmann::json output;
+
+	for (const auto &[folder, content]: json.items())
+	{
+		if (!output.contains(folder))
+		{
+			output[folder] = nlohmann::json::object();
+		}
+
+		for (const auto &[filename, data]: content.items())
+		{
+			const auto filesize = data.get<std::vector<char>>().size();
+			output[folder][filename] = std::format("<{} bytes>", filesize);
+		}
+	}
+
+	std::cout << output.dump(4) << '\n';
+	return 0;
+}
+
+auto main(const int argc, const char **argv) -> int
+{
+	if (argc <= 2)
+	{
+		print_help(argv[0]);
+		return 1;
+	}
+
+	if (std::strcmp(argv[1], "-p") == 0 || std::strcmp(argv[1], "--pack") == 0)
+	{
+		return pack(argv[2]);
+	}
+
+	if (std::strcmp(argv[1], "-u") == 0 || std::strcmp(argv[1], "--unpack") == 0)
+	{
+		return unpack(argv[2]);
+	}
+
+	if (std::strcmp(argv[1], "-o") == 0 || std::strcmp(argv[1], "--output") == 0)
+	{
+		return output(argv[2]);
+	}
+
+	print_help(argv[0]);
+	return 1;
+}
