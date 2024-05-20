9f97bd8447bd4fc82449e2bd549ef97111f4e956
kraxarn <kraxie@protonmail.com>
2024-05-20 22:39:56 +0200

    Move animated sprite logic to chirp

---
+++ include/chirp/animatedsprite.hpp
@@ -0,0 +1,64 @@
+#pragma once
+
+#include <chirp/asset.hpp>
+#include <chirp/color.hpp>
+#include <chirp/direction.hpp>
+#include <chirp/rectangle.hpp>
+#include <chirp/tileset.hpp>
+
+namespace chirp
+{
+	class animated_sprite
+	{
+	public:
+		explicit animated_sprite(const asset<tileset> &tileset);
+
+		void update(float delta);
+
+		void draw() const;
+		void debug_draw(const color &color) const;
+
+		[[nodiscard]]
+		auto get_scale() const -> float;
+
+		void set_scale(float value);
+
+		[[nodiscard]]
+		auto get_direction() const -> direction;
+
+		void set_direction(direction value);
+
+		[[nodiscard]]
+		auto get_position() const -> const vector2f &;
+
+		void set_position(const vector2f &value);
+
+		void play();
+		void pause();
+
+		[[nodiscard]]
+		auto get_shape() const -> rectangle<float>;
+
+		void set_frame(int value);
+
+		void flip_horizontal();
+
+	private:
+		static constexpr float default_frame_time = 1.F / 60.F * 10.F;
+		static constexpr auto default_scale = 1.F;
+		static constexpr auto default_rotation = 0.F;
+		static constexpr auto default_direction = direction::left;
+
+		float frame_time;
+		float current_time;
+		float scale;
+		float rotation;
+
+		bool playing;
+		int current_frame;
+
+		asset<tileset> tileset;
+		direction direction;
+		vector2f position;
+	};
+}
---
+++ src/animatedsprite.cpp
@@ -0,0 +1,113 @@
+#include "chirp/animatedsprite.hpp"
+
+#include "chirp/internal/utils.hpp"
+
+#include "raylib.h"
+
+chirp::animated_sprite::animated_sprite(const asset<chirp::tileset> &tileset)
+	: frame_time(default_frame_time),
+	current_time(0.F),
+	scale(default_scale),
+	rotation(default_rotation),
+	playing(true),
+	current_frame(0),
+	tileset(tileset),
+	direction(default_direction)
+{
+}
+
+void chirp::animated_sprite::update(const float delta)
+{
+	if (!playing)
+	{
+		return;
+	}
+
+	current_time += delta;
+	if (current_time < frame_time)
+	{
+		return;
+	}
+
+	current_time = 0.F;
+	current_frame++;
+
+	if (current_frame >= tileset->get_size())
+	{
+		current_frame = 0;
+	}
+}
+
+void chirp::animated_sprite::draw() const
+{
+	tileset->draw(get_position(), current_frame, rotation, scale);
+}
+
+void chirp::animated_sprite::debug_draw(const color &color) const
+{
+	const auto shape = get_shape().to<int>();
+	const auto r_color = internal::utils::to_r_color(color);
+
+	DrawRectangleLines(shape.x(), shape.y(), shape.width(), shape.height(), r_color);
+}
+
+auto chirp::animated_sprite::get_scale() const -> float
+{
+	return scale;
+}
+
+void chirp::animated_sprite::set_scale(const float value)
+{
+	scale = value;
+}
+
+auto chirp::animated_sprite::get_direction() const -> chirp::direction
+{
+	return direction;
+}
+
+void chirp::animated_sprite::set_direction(const chirp::direction value)
+{
+	direction = value;
+}
+
+auto chirp::animated_sprite::get_position() const -> const vector2f &
+{
+	return position;
+}
+
+void chirp::animated_sprite::set_position(const vector2f &value)
+{
+	position = value;
+}
+
+void chirp::animated_sprite::play()
+{
+	playing = true;
+}
+
+void chirp::animated_sprite::pause()
+{
+	playing = false;
+}
+
+auto chirp::animated_sprite::get_shape() const -> rectangle<float>
+{
+	const auto &pos = get_position();
+	const auto size = static_cast<float>(tileset->get_size()) * get_scale();
+	return {pos.x(), pos.y(), size, size};
+}
+
+void chirp::animated_sprite::set_frame(const int value)
+{
+	current_frame = value;
+}
+
+void chirp::animated_sprite::flip_horizontal()
+{
+	tileset->flip_horizontal();
+
+	set_direction(get_direction() == direction::left
+		? direction::right
+		: direction::left);
+}
