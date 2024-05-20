d3d571b13eef1846d104c965bcdb717cf1d6824f
kraxarn <kraxie@protonmail.com>
2024-05-20 22:40:39 +0200

    Add utility for converting a rectangle

--- include/chirp/internal/utils.hpp
+++ include/chirp/internal/utils.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "chirp/color.hpp"
+#include "chirp/rectangle.hpp"
 #include "chirp/vector2.hpp"
 
 #include "raylib.h"
@@ -27,6 +28,17 @@ namespace chirp::internal
 			return {
 				static_cast<T>(vec.x),
 				static_cast<T>(vec.y),
+			};
+		}
+
+		template<typename T>
+		static auto to_r_rectangle(const rectangle<T> &rec) -> Rectangle
+		{
+			return {
+				.x = static_cast<float>(rec.x()),
+				.y = static_cast<float>(rec.y()),
+				.width = static_cast<float>(rec.width()),
+				.height = static_cast<float>(rec.height()),
 			};
 		}
 
