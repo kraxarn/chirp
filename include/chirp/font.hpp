35ed0f22486aa2552902b9e621003b5c51d2f9cd
kraxarn <kraxie@protonmail.com>
2024-06-02 15:12:45 +0200

    More namespace fixes

--- include/chirp/font.hpp
+++ include/chirp/font.hpp
@@ -12,10 +12,10 @@ namespace chirp
 		explicit font(const std::vector<unsigned char> &data, int font_size);
 		~font();
 
-		void draw_text(const text &text) const;
+		void draw_text(const chirp::text &text) const;
 
 		[[nodiscard]]
-		auto text_size(const text &text) const -> vector2f;
+		auto text_size(const chirp::text &text) const -> vector2f;
 
 		[[nodiscard]]
 		auto font_size() const -> int;
