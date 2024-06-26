999d985a2a81433659e3ee3016431bbabcf0dfc1
kraxarn <kraxie@protonmail.com>
2024-06-26 17:14:10 +0200

    Update pocketpy to v1.4.6

--- deps/pocketpy.cmake
+++ deps/pocketpy.cmake
@@ -2,7 +2,7 @@ include(FetchContent)
 
 FetchContent_Declare(pocketpy
 	GIT_REPOSITORY https://github.com/pocketpy/pocketpy.git
-	GIT_TAG 02b922a93eccb42d6d714f10e4539ad53b766ae1)
+	GIT_TAG v1.4.6)
 
 message(STATUS "Downloading: pocketpy")
 FetchContent_MakeAvailable(pocketpy)
