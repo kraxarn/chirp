950026a8f2eab9fab1c22cba813dbb2ff6c18426
kraxarn <kraxie@protonmail.com>
2024-06-26 16:55:19 +0200

    Change stubs to properties

--- stubs/chirp/game.pyi
+++ stubs/chirp/game.pyi
@@ -13,20 +13,26 @@ class Game:
 	def exit(self) -> None:
 		...
 
-	def get_target_fps(self) -> int:
+	@property
+	def target_fps(self) -> int:
 		...
 
-	def set_target_fps(self, fps: int) -> None:
+	@target_fps.setter
+	def target_fps(self, fps: int) -> None:
 		...
 
-	def get_clear_color(self) -> typing.Any:
+	@property
+	def clear_color(self) -> typing.Any:
 		...
 
-	def set_clear_color(self, color: typing.Any) -> None:
+	@clear_color.setter
+	def clear_color(self, color: typing.Any) -> None:
 		...
 
+	@property
 	def window(self) -> chirp.Window:
 		...
 
+	@property
 	def scenes(self) -> chirp.SceneManager:
 		...
