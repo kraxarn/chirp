a3f5cce84eb79b6363b6d79f5a0190eb4725e62a
kraxarn <kraxie@protonmail.com>
2024-06-29 18:58:50 +0200

    Fix assets stubs

--- stubs/chirp/assets.pyi
+++ stubs/chirp/assets.pyi
@@ -1,21 +1,22 @@
 import typing
+import chirp
 
 
 class Assets:
-	def music(self, path: str) -> typing.Any:
+	def music(self, path: str) -> chirp.Music:
 		...
 
-	def sound(self, path: str) -> typing.Any:
+	def sound(self, path: str) -> chirp.Sound:
 		...
 
-	def font(self, path: str, font_size: int) -> typing.Any:
+	def font(self, path: str, font_size: int) -> chirp.Font:
 		...
 
-	def image(self, path: str) -> typing.Any:
+	def image(self, path: str) -> chirp.Image:
 		...
 
 	def tileset(self, path: str) -> typing.Any:
 		...
 
-	def level(self, path: str) -> typing.Any:
+	def level(self, path: str) -> chirp.Level:
 		...
