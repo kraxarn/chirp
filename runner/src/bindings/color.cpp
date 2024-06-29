a30232ba10c9181963b1d3974a1b1c5827940a05
kraxarn <kraxie@protonmail.com>
2024-06-29 15:25:57 +0200

    Update color bindings

--- runner/src/bindings/color.cpp
+++ runner/src/bindings/color.cpp
@@ -3,60 +3,18 @@
 #include <chirp/color.hpp>
 #include <chirp/format.hpp>
 
-void bind_color(pkpy::VM *py_vm, pkpy::PyVar mod)
+void bind_color(const pybind11::module_ &module)
 {
-	py_vm->register_user_class<chirp::color>(mod, "Color",
-		[](pkpy::VM *pkpy_vm, pkpy::PyVar /*mod*/, const pkpy::PyVar type)
+	pybind11::class_<chirp::color>(module, "Color")
+		.def(pybind11::init<>())
+		.def(pybind11::init<unsigned char, unsigned char, unsigned char>())
+		.def(pybind11::init<unsigned char, unsigned char, unsigned char, unsigned char>())
+		.def_property_readonly("red", &chirp::color::red)
+		.def_property_readonly("green", &chirp::color::green)
+		.def_property_readonly("blue", &chirp::color::blue)
+		.def_property_readonly("alpha", &chirp::color::alpha)
+		.def("__str__", [](const chirp::color &color)
 		{
-			pkpy_vm->bind(type, "__init__(self, red, green, blue, alpha=255)",
-				[](pkpy::VM *py_vm, const pkpy::ArgsView args) -> pkpy::PyVar
-				{
-					auto &self = pkpy::py_cast<chirp::color &>(py_vm, args[0]);
-
-					const auto red = pkpy::py_cast<int>(py_vm, args[1]);
-					const auto green = pkpy::py_cast<int>(py_vm, args[2]);
-					const auto blue = pkpy::py_cast<int>(py_vm, args[3]);
-					const auto alpha = pkpy::py_cast<int>(py_vm, args[4]);
-
-					self = chirp::color(red, green, blue, alpha);
-					return py_vm->None;
-				});
-
-			pkpy_vm->bind(type, "__str__(self)", [](pkpy::VM *py_vm, const pkpy::ArgsView args) -> pkpy::PyVar
-			{
-				const auto &self = pkpy::py_cast<chirp::color &>(py_vm, args[0]);
-
-				pkpy::Dict dict(py_vm);
-				dict.set(py_var(py_vm, "red"), py_var(py_vm, self.red()));
-				dict.set(py_var(py_vm, "green"), py_var(py_vm, self.green()));
-				dict.set(py_var(py_vm, "blue"), py_var(py_vm, self.blue()));
-				dict.set(py_var(py_vm, "alpha"), py_var(py_vm, self.alpha()));
-
-				return py_var(py_vm, py_vm->py_str(py_var(py_vm, dict)));
-			});
-
-			pkpy_vm->bind(type, "red(self)", [](pkpy::VM *py_vm, const pkpy::ArgsView args) -> pkpy::PyVar
-			{
-				const auto &self = pkpy::py_cast<chirp::color &>(py_vm, args[0]);
-				return py_var(py_vm, self.red());
-			});
-
-			pkpy_vm->bind(type, "green(self)", [](pkpy::VM *py_vm, const pkpy::ArgsView args) -> pkpy::PyVar
-			{
-				const auto &self = pkpy::py_cast<chirp::color &>(py_vm, args[0]);
-				return py_var(py_vm, self.green());
-			});
-
-			pkpy_vm->bind(type, "blue(self)", [](pkpy::VM *py_vm, const pkpy::ArgsView args) -> pkpy::PyVar
-			{
-				const auto &self = pkpy::py_cast<chirp::color &>(py_vm, args[0]);
-				return py_var(py_vm, self.blue());
-			});
-
-			pkpy_vm->bind(type, "alpha(self)", [](pkpy::VM *py_vm, const pkpy::ArgsView args) -> pkpy::PyVar
-			{
-				const auto &self = pkpy::py_cast<chirp::color &>(py_vm, args[0]);
-				return py_var(py_vm, self.alpha());
-			});
-		}, pkpy::VM::tp_object, true);
+			return chirp::fmt::to_string(color);
+		});
 }
