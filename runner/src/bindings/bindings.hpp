d77a4178aef6b3b03c102ecaa5d90c626eb566b2
kraxarn <kraxie@protonmail.com>
2024-06-29 15:55:38 +0200

    Move os to own section

--- runner/src/bindings/bindings.hpp
+++ runner/src/bindings/bindings.hpp
@@ -11,10 +11,13 @@ namespace pybind11
 	using pyclass = class_<T>;
 }
 
+// chirp
 void bind_color(const pybind11::module &module);
 void bind_game(const pybind11::module &module);
-void bind_os(pybind11::module &module);
 void bind_scene(const pybind11::module &module);
 void bind_scene_manager(const pybind11::module &module);
 void bind_vector2(const pybind11::module &module);
 void bind_window(const pybind11::module &module);
+
+// chirp.os
+void bind_os(pybind11::module &module);
