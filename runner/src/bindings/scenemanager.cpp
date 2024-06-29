56fc8a3778685470cc46abce911d28ded72ac5a0
kraxarn <kraxie@protonmail.com>
2024-06-29 15:37:16 +0200

    Update scene manager bindings

--- runner/src/bindings/scenemanager.cpp
+++ runner/src/bindings/scenemanager.cpp
@@ -2,34 +2,13 @@
 
 #include <chirp/scenemanager.hpp>
 
-void bind_scene_manager(pkpy::VM *py_vm, pkpy::PyVar mod)
+void bind_scene_manager(const pybind11::module &module)
 {
-	py_vm->register_user_class<chirp::scene_manager>(mod, "SceneManager",
-		[](pkpy::VM *pkpy_vm, pkpy::PyVar /*mod*/, const pkpy::PyVar type)
-		{
-			pkpy_vm->bind(type, "push(self, scene)",
-				[](pkpy::VM *py_vm, const pkpy::ArgsView args) -> pkpy::PyVar
-				{
-					auto &self = pkpy::py_cast<chirp::scene_manager &>(py_vm, args[0]);
-					auto scene_type = args[1];
-					py_vm->call(scene_type, pkpy::py_var(py_vm, self.get_assets()));
-					// TODO
-					return py_vm->None;
-				});
-
-			pkpy_vm->bind(type, "queue_pop(self)",
-				[](pkpy::VM *py_vm, const pkpy::ArgsView args) -> pkpy::PyVar
-				{
-					auto &self = pkpy::py_cast<chirp::scene_manager &>(py_vm, args[0]);
-					self.queue_pop();
-					return py_vm->None;
-				});
-
-			pkpy_vm->bind(type, "peek(self)",
-				[](pkpy::VM *py_vm, const pkpy::ArgsView args) -> pkpy::PyVar
-				{
-					const auto &self = pkpy::py_cast<chirp::scene_manager &>(py_vm, args[0]);
-					return py_var(py_vm, self.peek());
-				});
-		}, pkpy::VM::tp_object, true);
+	pybind11::class_<chirp::scene_manager>(module, "SceneManager")
+		.def(pybind11::init<>())
+		.def("push", &chirp::scene_manager::push<chirp::scene>)
+		.def("queue_push", &chirp::scene_manager::queue_push)
+		.def("queue_pop", &chirp::scene_manager::queue_pop)
+		.def("empty", &chirp::scene_manager::empty)
+		.def("peek", &chirp::scene_manager::peek);
 }
