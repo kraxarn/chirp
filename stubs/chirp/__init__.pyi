50dc2147192c33b44dbc4aaffb7e6a96c0aeb78a
kraxarn <kraxie@protonmail.com>
2024-06-29 18:58:05 +0200

    Fix missing imports

--- stubs/chirp/__init__.pyi
+++ stubs/chirp/__init__.pyi
@@ -7,3 +7,8 @@ from .assets import Assets as Assets
 from .vector2 import Vector2i as Vector2i
 from .vector2 import Vector2f as Vector2f
 from .color import Color as Color
+from .font import Font as Font
+from .image import Image as Image
+from .music import Music as Music
+from .sound import Sound as Sound
+from .level import Level as Level
