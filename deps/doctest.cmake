cccbbc9f02d8185701cdcca6b87f861be2a4f963
kraxarn <kraxie@protonmail.com>
2025-12-17 17:31:08 +0100

    Remove unused deps

--- deps/doctest.cmake
+++
@@ -1,8 +0,0 @@
-include(FetchContent)
-
-FetchContent_Declare(doctest
-	GIT_REPOSITORY https://github.com/doctest/doctest.git
-	GIT_TAG v2.4.11)
-
-message(STATUS "Downloading: doctest")
-FetchContent_MakeAvailable(doctest)
